ded87b81999be4a9b8db66fd8768835bba3c6d3a
unknown <wallacekid1996@gmail.com>
2021-04-08 04:43:22 -0600

    Dragon Lair lab

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const authCtrl = require('./controllers/authController')
+const treasureCtrl = require('./controllers/treasureController')
+const auth = require('./middleware/authMiddleware');
+const express = require('express');
+const session = require('express-session');
+const massive = require('massive');
+
+const PORT = 4000;
+
+const app = express()
+const {CONNECTION_STRING, SESSION_SECRET} = process.env;
+
+app.use(express.json());
+
+app.post('/auth/register', authCtrl.register);
+app.post('/auth/login', authCtrl.login);
+app.get('/auth/logout', authCtrl.logout);
+
+
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure);
+app.get('/api/treasure/user', auth.usersOnly,treasureCtrl.getUserTreasure);
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure);
+app.get('/api/treasure/all', auth.usersOnly, auth.adminsOnly, treasureCtrl.getAllTreasure);
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then (db=> {
+    app.set('db', db);    
+});
+
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: false,
+        secret: SESSION_SECRET,
+    })
+);
+
+
+
+app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`))
